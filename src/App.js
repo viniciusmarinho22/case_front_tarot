73b2d10be1217f6e8fd23d1b6e6b831006bc99ea
ViniciusMarinho <viniciusandrem22@gmail.com>
2023-03-03 15:54:25 -0300

    inicio case front

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const cartas = require("../data/products")
+  
+  return (
+    <div className="App">
+      <h1>TAROT</h1>
+    </div>
+  );
+}
+
+export default App;
